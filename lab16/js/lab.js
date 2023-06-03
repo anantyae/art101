a7541ff8c13d7dc5ea06498f7cb08eea7e29ef3f
Anantya <aaengine@ucsc.edu>
2023-06-02 17:53:31 -0700

    lab 16 test something

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -12,17 +12,45 @@
         //what
       data: {},
       dataType: "json",
-      success: function(data){
+    })
+
+      
+      .done(function(data) {
 
           //success callback function
           console.log("success");
           console.log(data);
-          var comicObj = data;
-          var imgURL = data.img;
-          $("#output").html("<h2>" + comicObj.title);
-          $("#output").append("img src=" + comicObj.img + "alt=" + comicObj.alt + "title='Image'>");
-          //$("#output").append("<img src=" + imgURL + ">");
-      }
-})
+          var title = data.title;
+          console.log("orig title:", title);
+          title = make_safe(title);
+          console.log("safe title:", title);
+          var alt = data.alt;
+          console.log("orig alt:", alt);
+          alt = make_safe(alt);
+          console.log("safe alt:", alt);
+          var comicNum = data.num;
+          var html = `<div id="imageblock">
+              <h2>${title}</h2>
+              <img src="${imageUrl}" title="${alt}"><br>
+              <button id="prev">Previous</button><button id="next">Next</button>
+            </div>`
+          // console.log("My new html: \n", html);
+          $("#output").html(html);
+    
+          // add event listener to new prev button
+          $("#prev").click(function(){
+            getComic(comicNum - 1);
+          });
+          // add event listener to new next button
+          $("#next").click(function(){
+            getComic(comicNum + 1);
+          });
+    //       var imageUrl = data.img;
+    //       var imgURL = data.img;
+    //       $("#output").html("<h2>" + comicObj.title);
+    //       $("#output").append("img src=" + comicObj.img + "alt=" + comicObj.alt + "title='Image'>");
+    //       //$("#output").append("<img src=" + imgURL + ">");
+    //   }
+)
   
 
