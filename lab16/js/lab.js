edd1d88e0b857f28a538ab4ce4df33067ce648f0
Anantya <aaengine@ucsc.edu>
2023-06-02 17:42:32 -0700

    lab 16

---
+++ lab16/js/lab.js
@@ -0,0 +1,28 @@
+// index.js - JSON and APIs
+// Author: Anantya
+// Date: 6/2/2023
+
+
+ 
+    $.ajax({
+       //where- string
+      url: "https://xkcd.com/info.0.json",
+       //how- string
+      type: "GET",
+        //what
+      data: {},
+      dataType: "json",
+      success: function(data){
+
+          //success callback function
+          console.log("success");
+          console.log(data);
+          var comicObj = data;
+          var imgURL = data.image;
+          $("#output").html("<h2>" + comicObj.title);
+          $("#output").append("img src=" + comicObj.img + "alt=" + comicObj.alt + "title='Image'>");
+          //$("#output").append("<img src=" + imgURL + ">");
+      }
+})
+  
+
