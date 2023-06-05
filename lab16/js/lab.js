291e200eec53caa6e90e1febed76f968d0f30a84
Anantya <aaengine@ucsc.edu>
2023-06-05 12:59:13 -0700

    update

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,25 +2,21 @@
 // Author: Anantya
 // Date: 6/2/2023
 
-
-    $.ajax({
-       //where- string
-      url: "https://xkcd.com/info.0.json",
-       //how- string
-      type: "GET",
-        //what
-      data: {},
-      dataType: "json",
-      success: function(data){
-      // If the request succeeds
-      console.log("success");
-      console.log(data);
-      var comicObj = data;
-      //
-
-
-
-$("#output").html("<h2>" + comicObj.title + "<h2>" );
-$("#output").append("img src=" + comicObj.img + "alt=" + comicObj.alt + "title='Image'>");
-  }
+$.ajax({
+    //url
+    url: "https://xkcd.com/info.0.json",
+    //want all of the data
+    data: {},
+    type: "GET",
+    dataType: "json",
+    success: function(data){
+        //success callback function (what code does with the retrieved data)
+        console.log("success");
+        console.log(data);
+        var comicObj = data;
+        //puts title on page
+        $("#output").append("<h3>" + comicObj.title + "</h3>");
+        //puts image on page, and attaches alt text & title to image
+        $("#output").append("<img src=" + comicObj.img + " alt=" + comicObj.alt + "title='Image'>");
+    }
 })
